d13e46afa0a28c87c033cfaae8c2bbd8b91b47f7
nhakkaou <noureddine.hak@gmail.com>
2022-06-22 18:46:00 +0100

    add Script to wp

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,4 @@
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+&& chmod +x wp-cli.phar \
+&& sudo mv wp-cli.phar /usr/local/bin/wp \
+&& wp core download && wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com
