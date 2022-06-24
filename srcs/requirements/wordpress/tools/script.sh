579a6f3df8df07e4ddfb0ef54c11f5253c5eacaa
nhakkaou <noureddine.hak@gmail.com>
2022-06-24 18:48:51 +0100

    Fix error

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Install Wp";
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
-chmod +x wp-cli.phar \
-mv wp-cli.phar /usr/local/bin/wp \
-wp core download --path=/home/wp --allow-root \
-rm -rf /home/wp/wp-config-sample.php
-mv /tmp/wp-config.php /home/wp/ \
-wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --path=/home/wp --allow-root
+rm -rf /home/wp/wp-config-sample
+mv /tmp/wp-config.php /home/wp/
+wp core install --url=${MYSQL_URL} --title=Example --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
 tail -f > /dev/null
