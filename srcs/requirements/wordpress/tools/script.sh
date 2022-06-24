8e23072f83804a0451f78b730cab0ac86bee9f96
nhakkaou <noureddine.hak@gmail.com>
2022-06-24 18:28:33 +0100

    Update

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,9 +2,10 @@
 echo "Install Wp";
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
-&& chmod +x wp-cli.phar \
-&& mv wp-cli.phar /usr/local/bin/wp \
-&& wp core download --path=/home/wp --allow-root \
-&& mv /tmp/wp-config.php /home/wp/ \
-&& wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/home/ --allow-root
+chmod +x wp-cli.phar \
+mv wp-cli.phar /usr/local/bin/wp \
+wp core download --path=/home/wp --allow-root \
+rm -rf /home/wp/wp-config-sample.php
+mv /tmp/wp-config.php /home/wp/ \
+wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
 tail -f > /dev/null
