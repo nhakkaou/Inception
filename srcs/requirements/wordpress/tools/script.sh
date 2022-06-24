baf1c62fb62ddfee59ae436b3abf22603966a5c0
nhakkaou <noureddine.hak@gmail.com>
2022-06-24 16:16:37 +0100

    switch to script

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,5 +3,8 @@ echo "Install Wp";
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
 && chmod +x wp-cli.phar \
-&& sudo mv wp-cli.phar /usr/local/bin/wp \
-&& wp core download && wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com
+&& mv wp-cli.phar /usr/local/bin/wp \
+&& wp core download --path=/home/ --allow-root \
+echo "Create wp config file";
+wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/home --allow-root \
+&& wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/home/ --allow-root
