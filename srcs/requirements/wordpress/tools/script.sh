fd31c274ca1650e64e11028b8217ce6c7468046d
nhakkaou <noureddine.hak@gmail.com>
2022-06-25 18:23:35 +0100

    Up

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,5 +7,5 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --path=/home/wp --allow-root
 rm -rf /home/wp/wp-config-sample
 mv /tmp/wp-config.php /home/wp/
-# wp core install --url=${MYSQL_URL} --title=Example --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
+wp core install --url=${MYSQL_URL} --title=Example --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
 tail -f > /dev/null
