dd2888e48476624a9398032f820f0e74583ddd81
nhakkaou <noureddine.hak@gmail.com>
2022-06-27 22:26:41 +0100

    Change wp path

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,8 +4,8 @@ echo "Install Wp";
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp core download --path=/home/wp --allow-root
-rm -rf /home/wp/wp-config-sample.php
-wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/home/wp/ --allow-root --skip-check
-wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/home/wp/ --allow-root
+wp core download --path=/var/www/html --allow-root
+rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/var/www/html/ --allow-root --skip-check
+wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/var/www/html/ --allow-root
 tail -f > /dev/null
