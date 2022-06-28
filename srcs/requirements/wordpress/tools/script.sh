13449acc0b5db45f90e41b43b55acc6b6c4e169e
nhakkaou <noureddine.hak@gmail.com>
2022-06-28 17:23:59 +0100

    fix yaml format docker compose

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,8 +4,9 @@ echo "Install Wp";
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp core download --path=/var/www/html/wordpress --allow-root
-rm -rf /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/var/www/html/wordpress/ --allow-root --skip-check
-wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/var/www/html/wordpress/ --allow-root
+wp core download --path=/var/www/html --allow-root
+chown -R www-data:www-data /var/www/html
+rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/var/www/html/ --allow-root --skip-check
+wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/var/www/html/ --allow-root
 tail -f > /dev/null
