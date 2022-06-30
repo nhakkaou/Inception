6640b208c29576b1c598cbf5bd920fb2a1934ed2
nhakkaou <noureddine.hak@gmail.com>
2022-06-30 15:47:30 +0100

    Change nginx config

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -10,3 +10,4 @@ rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/var/www/html/ --allow-root --skip-check
 wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/var/www/html/ --allow-root
 tail -f > /dev/null
+# tail is forbiden just for test
