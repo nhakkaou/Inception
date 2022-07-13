07614950fdee03bf188df4db0adfd727c54e20bb
Noreddine Hakkaoui <nhakkaou@e2r9p8.1337.ma>
2022-07-13 20:08:37 +0100

    Fix wordpress error

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,5 +9,6 @@ chown -R www-data:www-data /var/www/html
 rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config create --dbname=nhakkaou --dbuser=nhakkaou --dbpass=toor --dbhost=mariadb --path=/var/www/html/ --allow-root --skip-check
 wp core install --url=nhakkaou.42.fr --title=Example --admin_user=nhakkaou --admin_password=toor --admin_email=noureddine.hak@gmail.com --path=/var/www/html/ --allow-root
+service php7.3-fpm start
 tail -f > /dev/null
 # tail is forbiden just for test
