c7d88ffdfa2d5d60823160ccb2ff6881e2c35e07
nhakkaou <noureddine.hak@gmail.com>
2022-07-19 19:28:35 +0100

    config ftp

---
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -0,0 +1,16 @@
+adduser --gecos "" --disabled-password sammy
+mkdir /home/sammy/ftp
+chown nobody:nogroup /home/sammy/ftp
+chmod a-w /home/sammy/ftp
+ls -la /home/sammy/ftp
+mkdir /home/sammy/ftp/files
+chown sammy:sammy /home/sammy/ftp/files
+ls -la /home/sammy/ftp
+echo "vsftpd test file" | sudo tee /home/sammy/ftp/files/test.txt
+echo "vsftpd test file" | tee /home/sammy/ftp/files/test.txt
+apt-get install vim
+vim  /etc/vsftpd.conf
+echo "sammy" | tee -a /etc/vsftpd.userlist
+cat /etc/vsftpd.userlist
+service vsftpd start
+tail -f /dev/null
