790cf2cc72f12ac2a409d0ec0e7cd74c85095431
nhakkaou <noureddine.hak@gmail.com>
2022-07-21 17:29:49 +0100

    Update docker compose

--- srcs/requirements/bonus/website/tools/script.sh
+++ srcs/requirements/bonus/website/tools/script.sh
@@ -1,1 +1,6 @@
+git clone https://github.com/nhakkaou/Pong-Game.git /var/www/html/Pong-Game
+chown -R www-data:www-data /var/www/html/Pong-Game
+chmod -R 755 /var/www/html/Pong-Game
+cd /var/www/html/Pong-Game
+npm install
 tail -f /dev/null
