6b03b1bf0e8961d7104d5e880282aa9dde0d4798
nhakkaou <noureddine.hak@gmail.com>
2022-07-22 18:23:36 +0100

    Add curl

--- srcs/requirements/bonus/website/tools/script.sh
+++ srcs/requirements/bonus/website/tools/script.sh
@@ -2,5 +2,5 @@ curl -sL https://deb.nodesource.com/setup_16.x |  bash -
 apt -y install nodejs
 git clone https://github.com/nhakkaou/Pong-Game.git /var/www/html/Pong-Game
 npm --prefix /var/www/html/Pong-Game/ install --force
-npm --prefix /var/www/html/Pong-Game/ start
+npm --prefix /var/www/html/Pong-Game/ run dev
 tail -f /dev/null
