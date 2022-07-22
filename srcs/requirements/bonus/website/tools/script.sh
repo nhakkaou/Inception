1e8de2461e5a6fcf6f05bfc291cd1e70c040ca3a
nhakkaou <noureddine.hak@gmail.com>
2022-07-22 17:47:34 +0100

    change script website

--- srcs/requirements/bonus/website/tools/script.sh
+++ srcs/requirements/bonus/website/tools/script.sh
@@ -1,9 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_16.x |  bash -
 apt -y install nodejs
 git clone https://github.com/nhakkaou/Pong-Game.git /var/www/html/Pong-Game
-cd /var/www/html/Pong-Game
-npm install
-npm install -g serve
-npm run build
-serve -s build
+npm --prefix /var/www/html/Pong-Game/ install --force
+npm --prefix /var/www/html/Pong-Game/ start
 tail -f /dev/null
