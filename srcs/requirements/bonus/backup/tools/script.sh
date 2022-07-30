37887cf7a18920166c00eb6689bc5b0c666bf924
nhakkaou <noureddine.hak@gmail.com>
2022-07-30 22:14:52 +0100

    backup

--- srcs/requirements/bonus/backup/tools/script.sh
+++ srcs/requirements/bonus/backup/tools/script.sh
@@ -1,2 +1,11 @@
 mkdir /home/backup/
-tail -f > /dev/null
+while(true)
+{
+    echo "Backup in progress..."
+    $tmp = /home/backup/`date +%F`
+    mkdir $tmp
+    cp -r /home/ $tmp
+    echo "Backup done."
+    #  backup every 4 hours
+    sleep 240
+}
