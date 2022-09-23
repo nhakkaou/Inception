e6ea3c32e3582d2bcd5438d1cb16bf2f126be8fd
Noreddine Hakkaoui <nhakkaou@e2r4p11.1337.ma>
2022-09-23 18:24:27 +0100

    Redis not connected

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,8 +9,9 @@ chown -R www-data:www-data /var/www/html
 rm -rf /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER_ROOT --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=/var/www/html/ --allow-root --skip-check
 wp core install --url=$MYSQL_URL --title=Example --admin_user=$MYSQL_USER_ROOT --admin_password=$MYSQL_PASSWORD --admin_email=noureddine.hak@gmail.com --path=/var/www/html/ --allow-root
-wp plugin install redis-cache --path=/var/www/html/wordpress --activate --allow-root
-wp plugin activate redis-cache --path=/var/www/html/ --allow-root
+# Install Redis cache plugin
+wp plugin install redis-cache --path=/var/www/html/ --activate --allow-root
+wp plugin install redis-cache --path=/var/www/html/ --activate --allow-root
 echo "define('WP_CACHE', true);" >> /var/www/html/wp-config.php
 echo "define('WP_REDIS_HOST', 'redis');" >> /var/www/html/wp-config.php
 service php7.3-fpm start
